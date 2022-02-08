41c224cbf22c06747ee2bc4864f307125a31225b
Stanislav Homych <stas.homich.sh@gmail.com>
2022-02-08 21:22:32 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
 import './App.css';
+import { fromEvent,  } from 'rxjs'; 
+import { map, buffer, debounceTime, filter } from 'rxjs/operators';
 
 function App() {
+
+  var button = document.getElementsByClassName('button')
+  const mouse = fromEvent(button, 'click')
+  
+  const buff = mouse.pipe(
+    debounceTime(300),
+  );
+  
+  const click = mouse.pipe(
+    buffer(buff),
+    map(arr => {
+      return arr.length;
+    }),
+    filter(x => x === 2),
+  );
+  
+  click.subscribe(() => {
+    setTimeOn(false)
+  });
+
+
+  const[timeStart, setTime] =  React.useState(0)
+  const[timerOn, setTimeOn] = React.useState(false)
+
+useEffect(()=>{
+  let interval 
+
+  if(timerOn){
+    interval = setInterval(()=>{
+      setTime(previousTime => previousTime + 10)
+    },10)
+  }else{
+    clearInterval(interval)
+  }
+  return()=> clearInterval(interval)
+}, [timerOn])
+
+
+ {
+   /*double click on 'Wait' button*/
+ }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <span>{("0" + Math.floor((timeStart / 60000) % 60))}:</span>
+            <span>{("0" + Math.floor((timeStart / 1000) % 60))}:</span>
+            <span>{("0" + Math.floor((timeStart / 10) % 100)).slice(-2)}</span>
+      <div>
+        {!timerOn && (
+           <button onClick={()=>setTimeOn(true)}>Start</button>
+
+        )}
+         {timerOn && (
+           <button onClick={()=>setTimeOn(false)}>Stop</button>
+
+        )}
+        <button className="button">Wait</button>
+        <button onClick={()=>setTime(0)}>Reset</button>
+        <h4></h4>
+
+      </div>
     </div>
   );
 }
